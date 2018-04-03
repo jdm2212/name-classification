5b4d19f694dfc6d0d7e1898a9467b16448d615b5
Jeff Martin <jmartin@palantir.com>
2018-04-02 22:47:30 -0400

    grab only the highest-scoring ethnicity

--- classify_names.py
+++ classify_names.py
@@ -15,10 +15,41 @@ def _load_batch(names):
     request = urllib.request.Request(API_URL, data=json_data, headers=HEADERS)
     response = urllib.request.urlopen(request)
     output = response.read()
-    print(output)
     json_result = json.loads(output) #.decode('utf8')
     return json_result
 
+def _highest_scoring_ethnicity(ethnicity_scores):
+    """
+    the ethnicity block looks like:
+    [{'score': '.05', ethnicity: 'foo'}, {'score': '0.4', ethnicity: 'bar'}]
+    """
+    best = {'score': -1, 'ethnicity': 'ERROR_ETHNICITY'}
+    for ethnicity_score in ethnicity_scores:
+        print(ethnicity_score)
+        if float(ethnicity_score['score']) > best['score']:
+            best['score'] = float(ethnicity_score['score'])
+            best['ethnicity'] = ethnicity_score['ethnicity']
+    return best
+
+def _highest_scores_for_name(name_scores):
+    print(name_scores)
+    continent = _highest_scoring_ethnicity(name_scores[0]['scores'])
+    region = _highest_scoring_ethnicity(name_scores[1]['scores'])
+    return [continent['ethnicity'],
+            continent['score'],
+            region['ethnicity'],
+            region['score']]
+
+def _load_names_and_get_highest_ethnicities(names):
+    batch_results = _load_batch(names)
+    print(batch_results)
+    results = {}
+    for name, scores in batch_results.items():
+        print("name:" + name)
+        print("scores:" + str(scores))
+        results[name] = _highest_scores_for_name(scores)
+    return results
 
 NAMES = ["George Washington", "Barack Obama"]
-print(_load_batch(NAMES)[NAMES[0]])
+output = _load_names_and_get_highest_ethnicities(NAMES)
+print(output)
