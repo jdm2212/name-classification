ce355a0c49477f6b8045c0e92f47ce16e7d1432e
Jeff Martin <jmartin@palantir.com>
2018-04-07 11:54:52 -0400

    csv-ify

--- v2/classify_names.py
+++ v2/classify_names.py
@@ -8,13 +8,27 @@ import sys
 import json
 import urllib.request
 import urllib.parse
+import csv
 
-API_URL = 'http://www.textmap.com/ethnicity_api/api'
-HEADERS = {'content-type': 'application/json'}
 CACHE_FILE = "cache.json"
+OUTPUT_FILE = "classified_names.csv"
 BATCH_SIZE = 2000
+NATIONALITY = "nat"
+ETHNICITY = "eth" 
 
 API_URL_TEMPLATE = Template('http://www.name-prism.com/api_token/$type/json/$apiToken/$urlEncodedName')
+
+HEADER = ["name", "nat:African:EastAfrican","nat:African:SouthAfrican","nat:African:WestAfrican","nat:CelticEnglish",
+          "nat:EastAsian:Chinese","nat:EastAsian:Indochina:Cambodia","nat:EastAsian:Indochina:Myanmar",
+          "nat:EastAsian:Indochina:Thailand","nat:EastAsian:Indochina:Vietnam","nat:EastAsian:Japan",
+          "nat:EastAsian:Malay:Indonesia","nat:EastAsian:Malay:Malaysia","nat:EastAsian:South Korea","nat:European:Baltics",
+          "nat:European:EastEuropean","nat:European:French","nat:European:German","nat:European:Italian:Italy",
+          "nat:European:Italian:Romania","nat:European:Russian","nat:European:SouthSlavs","nat:Greek",
+          "nat:Hispanic:Philippines","nat:Hispanic:Portuguese","nat:Hispanic:Spanish","nat:Jewish",
+          "nat:Muslim:ArabianPeninsula","nat:Muslim:Maghreb","nat:Muslim:Nubian","nat:Muslim:Pakistanis:Bangladesh",
+          "nat:Muslim:Pakistanis:Pakistan","nat:Muslim:Persian","nat:Muslim:Turkic:CentralAsian","nat:Muslim:Turkic:Turkey",
+          "nat:Nordic:Finland","nat:Nordic:Scandinavian:Denmark","nat:Nordic:Scandinavian:Norway","nat:Nordic:Scandinavian:Sweden",
+          "nat:SouthAsian","eth:2PRACE","eth:AIAN","eth:API","eth:Black","eth:Hispanic","eth:White"]
 
 def _load_name_type(name, apiToken, type):
     urlEncodedName = urllib.parse.quote_plus(name)
@@ -27,8 +41,8 @@ def _load_name_type(name, apiToken, type):
 
 def _load_name(name, apiToken):
     return {
-        "nat": _load_name_type(name, apiToken, "nat"),
-        "eth": _load_name_type(name, apiToken, "eth")
+        NATIONALITY: _load_name_type(name, apiToken, NATIONALITY),
+        ETHNICITY: _load_name_type(name, apiToken, ETHNICITY)
     }
 
 def _load_batch(names, apiToken):
@@ -55,6 +69,8 @@ def _load_batch_and_cache(names, apiToken):
     for name  in names:
         if name not in cache:
             names_to_load.append(name)
+    print("batch size: " + str(len(names)) + ", cache misses: ", str(len(names_to_load)))
+
     loaded = _load_batch(names_to_load, apiToken)
     new_cache = {**loaded, **cache}
     pretty_printed_cache = json.dumps(new_cache, indent=4, sort_keys=True)
@@ -69,13 +85,33 @@ def _load_batch_and_cache(names, apiToken):
     return values_to_return
 
 def _load_all(names, apiToken):
+    print("====================================================")
     index = 0
     while index < len(names):
         next_index = min(index + BATCH_SIZE, len(names))
+        print("next batch: names[" + str(index) + ":" + str(next_index) + "]")
         next_batch = names[index:next_index]
         _load_batch_and_cache(next_batch, apiToken)
         index = next_index
+    print("====================================================")
+    print("loading everything... should just be from the cache")
     return _load_batch_and_cache(names, apiToken)
+
+def _classified_name_to_csv_row(name, classification):
+    row = [name]
+    for value in classification[NATIONALITY].values():
+        row.append(value)
+    for value in classification[ETHNICITY].values():
+        row.append(value)
+    return row
+
+def _print_csv(classifiedNames):
+    with open(OUTPUT_FILE, 'w') as csvfile:
+        csvwriter = csv.writer(csvfile)
+        csvwriter.writerow(HEADER)
+        for name, classification in classifiedNames.items():
+            csvwriter.writerow(_classified_name_to_csv_row(name, classification))
+
 
 if len(sys.argv) is not 2:
     print("Usage: python classify_names.py API_TOKEN")
@@ -85,4 +121,4 @@ apiToken = sys.argv[1]
 
 NAMES = ["George Washington", "Barack Obama", "Xi Jinping", "Janet Lu", "Jeffrey Martin"]
 output = _load_all(NAMES, apiToken)
-print(output)
+_print_csv(output)
